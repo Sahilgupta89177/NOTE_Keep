9761e9c7542741dce7ffd7e44a742bcd147d4aa8
Sahil <sahilgupta89177@gmail.com>
2022-10-15 23:48:24 +0530

    Keep Note With You

--- app/src/main/java/com/example/note_app_gdsc/MainActivity.kt
+++ app/src/main/java/com/example/note_app_gdsc/MainActivity.kt
@@ -7,33 +7,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.note_app_gdsc.data.NotesDataSource
-import com.example.note_app_gdsc.model.Note
 import com.example.note_app_gdsc.screen.NoteScreen
 import com.example.note_app_gdsc.screen.NoteViewModel
 import com.example.note_app_gdsc.ui.theme.NOTE_APP_GDSCTheme
-import java.time.format.DateTimeFormatter
 
 class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalComposeUiApi::class)
